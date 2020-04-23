f8bb3f95a0cdf9760a8a2950cf0146f8c65783b8
rjs24 <rjseacome@gmail.com>
2020-04-23 15:11:51 +0100

    Changing way of iterating through paths with extra breaks for ftp server recover

--- publisher.py
+++ publisher.py
@@ -2,6 +2,8 @@ import ftplib
 import time
 import pika
 import os
+import random
+import time
 
 rabbit_conn = pika.BlockingConnection(pika.ConnectionParameters(os.environ.get('RABBITMQ_HOST')))
 channel = rabbit_conn.channel()
@@ -15,33 +17,49 @@ def connector():
     #ftp_connection.set_pasv(True)
     return ftp_connection
 
+def file_opener():
+    with open("dirslist.txt", "r") as file:
+        dirs_list = file.readlines()
+        return dirs_list
 
 def parser():
-    conn = connector()
-    conn.cwd("/pub/databases/pmc/suppl/NON-OA")
-    dirs = conn.nlst()
-    conn.close()
+    dirs = file_opener()
+    print(dirs)
+    max_index = len(dirs)
+    done_list = []
+    counter = 0
 
-    for d in dirs:
-        time.sleep(20)
-        try:
-            new_conn = ftplib.FTP("ftp.ebi.ac.uk")
-            new_conn.login()
-            new_conn_location = "/pub/databases/pmc/suppl/NON-OA/%s" % d
-            new_conn.cwd(new_conn_location)
-            print("new ftp connection now", new_conn)
-            new_dir = new_conn.pwd()
-            files_list = new_conn.nlst()
-            for files in files_list:
-                new_path = new_dir + '/' + files
-                channel.basic_publish(exchange='', routing_key='ftp_paths', body=new_path)
-                if files == files_list[-1]:
-                    new_conn.close()
-                    break
-                else:
+    while len(done_list) != max_index:
+        if counter %5 == 0:
+            random_sleep = random.randint(1800, 5400)
+            time.sleep(random_sleep)
+            continue
+        else:
+            counter += 1
+            current_list = [x for x in dirs if x not in done_list]
+            print(len(done_list), len(current_list))
+            current_target = random.choice(current_list)
+            try:
+                new_conn = connector()
+                new_conn_location = "/pub/databases/pmc/suppl/NON-OA/%s" % current_target
+                new_conn.cwd(new_conn_location)
+                print("new ftp connection now", new_conn)
+                current_path = new_conn.pwd()
+                files_list = new_conn.nlst()
+                if files_list == []:
+                    done_list.append('empty_list')
                     continue
-        except Exception as e:
-            print(e)
+                else:
+                    for files in files_list:
+                        new_path = current_path + '/' + files
+                        channel.basic_publish(exchange='', routing_key='ftp_paths', body=new_path)
+                        if files == files_list[-1]:
+                            new_conn.close()
+                            break
+                        else:
+                            continue
+            except Exception as e:
+                print(e)
 
 if __name__ == '__main__':
     parser()
